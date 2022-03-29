18adb853ab048aa5a5fe5b00706567c876a1992f
vishnu <vishnumaheendran1@gmail.com>
2022-03-29 18:18:20 +0530

    Add JWT auth, middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser,loginUser,getMe}  = require('../controllers/userController');
+const {registerUser,loginUser,getMe,getAllUsers}  = require('../controllers/userController');
+const {protect} = require('../middleware/authMiddleware');
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getMe);
+router.get('/me',protect, getMe);
+router.get('/', getAllUsers);
 module.exports = router;
