669e29bfad506d75595fae752f88b6db6f271622
vishnu <vishnumaheendran1@gmail.com>
2022-03-30 10:31:53 +0530

    Add Authorisation to routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require('express-async-handler')
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler (async (req,res) =>{
-    const goals  = await Goal.find();
+    const goals  = await Goal.find({user:req.user.id});
     res.status(200).json(goals);
 })
 
@@ -19,7 +20,8 @@ const setGoals = asyncHandler( async (req,res) =>{
     } 
 
     const goal = await Goal.create({
-        text:req.body.text
+        text:req.body.text,
+        user:req.user.id
     })
     res.status(200).json(goal)
 })
@@ -34,6 +36,21 @@ const updateGoals = asyncHandler( async (req,res) =>{
        res.status(400);
        throw new Error('No record found!')
    }
+   const user = await User.findById(req.user.id);
+   
+   //check for user
+   if(!user){
+    req.status(401);
+    throw new Error('User not authorised');
+   }
+
+   //Verify logged in user matches the goal user
+   if(goal.user.toString() != user.id){
+    res.status(401);
+    throw new Error('User not authorised');
+   }
+
+
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
       new:true
   })
@@ -49,6 +66,20 @@ const deleteGoals = asyncHandler (async (req,res) =>{
     if(!goal){
         req.status(400);
         throw new Error('No record found');
+    }
+
+    const user = await User.findById(req.user.id);
+   
+    //check for user
+    if(!user){
+     req.status(401);
+     throw new Error('User not authorised');
+    }
+ 
+    //Verify logged in user matches the goal user
+    if(goal.user.toString() != user.id){
+     res.status(401);
+     throw new Error('User not authorised');
     }
 
     //const deletedGoal = await Goal.findByIdAndDelete(req.params.id, req.body);
