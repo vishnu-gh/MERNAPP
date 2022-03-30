b914a32137cde72fc9ffe0dfe740696918db6009
vishnu <vishnumaheendran1@gmail.com>
2022-03-30 12:06:48 +0530

    Add React frontend app

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {useState,useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+function Login(){
+    const [formData,setFormData] = useState({      
+        email:'',
+        password:''       
+    });
+    const {email,password} = formData;
+    
+    const onChange =(e)=>{
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+
+    }
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+    }
+
+    return(
+        <>
+        <section className="heading">
+            <h1>
+            <FaSignInAlt /> Login
+            </h1>
+            <p>Please login to your account</p>
+        </section>
+
+        <section className='form'>
+            <form onSubmit={onSubmit}> 
+                <div className="form-group">
+                <input type="email" className='form-control' id="email" value={email} placeholder="Enter your email" onChange={onchange}></input>
+                </div>
+                <div className="form-group">
+                <input type="password" className='form-control' id="password" value={password} placeholder="Enter a password" onChange={onchange}></input>
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-block">Login</button>
+                </div>
+            </form>
+        </section>
+        </>
+    )
+}
+
+export default Login
